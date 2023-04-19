4bd9a5bc04a9ef1f114696f70b00861baee0a7d2
Eduardo <lauer.edu@gmail.com>
2023-04-19 16:16:49 -0300

    express.static('./public') added

--- starter/app.js
+++ starter/app.js
@@ -7,12 +7,10 @@ const port = 5000
 const app = express()
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
-app.get('/', (req, res) => {
-    res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
 
