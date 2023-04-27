0cb9509f20ea79b223d27c4032568d41c486946b
Eduardo <lauer.edu@gmail.com>
2023-04-27 15:56:26 -0300

    added features to getAllProductsStatic and getAllProducts

--- 02-Store-API/controllers/products.js
+++ 02-Store-API/controllers/products.js
@@ -1,9 +1,43 @@
+const Products = require('../models/products')
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: 'products testing route' })
+    const products = await Products.find({})
+    res.status(200).json({ products })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' })
+    const { featured, company, name, sort, fields } = req.query
+    const queryOject = {}
+
+    if (featured) {
+        queryOject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryOject.company = company
+    }
+
+    if (name) {
+        queryOject.name = { $regex: name, $options: 'i' }
+    }
+
+    let result = await Products.find(queryOject).sort()
+    
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+    const products = await result
+
+    res.status(200).json({ products })
 }
 
 module.exports = {
