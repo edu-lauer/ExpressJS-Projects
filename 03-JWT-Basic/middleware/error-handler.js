c0527c8520c1d6f7c959df6ba54c7bbca3864a3d
Eduardo <lauer.edu@gmail.com>
2023-05-01 08:54:57 -0300

    custom error

--- 03-JWT-Basic/middleware/error-handler.js
+++ 03-JWT-Basic/middleware/error-handler.js
@@ -1,4 +1,10 @@
+const CustomAPIError = require('../errors/custom-error')
+
+
 const errorHandlerMIddleware = async (err, req, res, next) => {
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({ msg: err.message })
+    }
     console.log(err)
     return res.status(500).json({ msg: 'Somenthing went wrong, please try again' })
 }
