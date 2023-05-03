4f3d410f33ce7733dd2f1fe36ae55c9687acb1af
Eduardo <lauer.edu@gmail.com>
2023-05-03 16:21:48 -0300

    Authentication

--- 03-JWT-Basic/controllers/main.js
+++ 03-JWT-Basic/controllers/main.js
@@ -11,13 +11,13 @@ const login = async (req, res) => {
     const id = new Date().getDate()
     // try to keep payload small, better experience for user
     const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
-
-    res.status(200).json({msg: 'user created', token})
+    res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({ msg: `Hello Edu`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
+
+    res.status(200).json({ msg: `Hello ${req.user.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
 }
 
 
