70dd511781d1017f15af2383b5021ddfb51b8f2d
Eduardo <lauer.edu@gmail.com>
2023-05-06 06:24:04 -0300

    updated code

--- 04-Jobs-API/app.js
+++ 04-Jobs-API/app.js
@@ -8,16 +8,22 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
+// error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use(notFoundMiddleware.js)
-app.use(errorHandlerMiddleware.js)
-
 // middleware
-app.use(express.static('./public'))
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+// app.use(express.static('./public'))
 app.use(express.json())
 
+// routes
+app.get('/', (req, res) => {
+    res.send('jobs api')
+})
 
 
 const start = async () => {
