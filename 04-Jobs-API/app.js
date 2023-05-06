53618205c61300ef06b41c176b03c856bedd0369
Eduardo <lauer.edu@gmail.com>
2023-05-06 07:23:23 -0300

    db connection fixed require('dotenv').config()

--- 04-Jobs-API/app.js
+++ 04-Jobs-API/app.js
@@ -1,5 +1,7 @@
-require('dotenv')
+require('dotenv').config()
 require('express-async-errors')
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 
 const connectDb = require('./db/connect')
 
@@ -12,30 +14,34 @@ const port = process.env.PORT || 5000
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+// app.use(express.static('./public'))
+app.use(express.json())
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1>')
+})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+
 // middleware
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 
-// app.use(express.static('./public'))
-app.use(express.json())
 
-// routes
-app.get('/', (req, res) => {
-    res.send('jobs api')
-})
-
-
-const start = async () => {
-    try {
-        // connectDb
-        await connectDb(process.env.MONGO_URI)
-        app.listen(port, () => {
-            console.log(`Server listening at http://localhost:${port}/`)
-        })
-    } catch (error) {
-        console.log(error)
+    const start = async () => {
+        try {
+            // connect to databased
+            await connectDb(process.env.MONGO_URI)
+            app.listen(port, () => {
+                console.log(`Server listening at http://localhost:${port}/`)
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
 
 start()
