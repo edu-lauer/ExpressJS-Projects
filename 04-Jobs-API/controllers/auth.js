4e65674c87a077745fc1bd9323df0ba4ab44dcde
Eduardo <lauer.edu@gmail.com>
2023-05-06 14:11:56 -0300

    jsonwebtoken implemented. Response with user.name and token

--- 04-Jobs-API/controllers/auth.js
+++ 04-Jobs-API/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors/index')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     // const { name, email, password } = req.body
@@ -9,7 +10,10 @@ const register = async (req, res) => {
     // }
 
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({ user })
+
+    const token = jwt.sign({ userId: user._id, name: user.name }, 'jswtSecret', { expiresIn: '30d' })
+
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
