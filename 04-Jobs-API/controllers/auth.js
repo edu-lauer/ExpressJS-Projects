2b255891650b05d5f663a5b44b784d0039e1f2d7
Eduardo <lauer.edu@gmail.com>
2023-05-07 10:43:07 -0300

    login validation

--- 04-Jobs-API/controllers/auth.js
+++ 04-Jobs-API/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors/index')
+const { BadRequestError, UnauthenticatedError } = require('../errors/index')
 
 const register = async (req, res) => {
     // const { name, email, password } = req.body
@@ -14,7 +14,27 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({ email })
+    
+    if (!user) {
+        throw new UnauthenticatedError('Invalid credentials')
+    }
+    
+    const isPasswordCorrect = await user.comparePassword(password)
+    console.log(isPasswordCorrect)
+
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid password')
+    }
+    
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 
