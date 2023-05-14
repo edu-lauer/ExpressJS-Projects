63b836ac57019b2d3f7c28c8fc318977442408c4
Eduardo <lauer.edu@gmail.com>
2023-05-14 07:04:26 -0300

    customError improved messages

--- 04-Jobs-API/middleware/error-handler.js
+++ 04-Jobs-API/middleware/error-handler.js
@@ -3,11 +3,23 @@ const { StatusCodes } = require('http-status-codes')
 
 
 const errorHandlerMIddleware = async (err, req, res, next) => {
+
+    let customError = {
+        // set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.msg || 'Something went wront try again later'
+    }
+
     if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({ msg: err.message })
     }
-    console.log(err)
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose anothe value`
+        customError.statusCode = 400
+    }
+
+    return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 
