4365129708d27cd1ad43837a665809dd5b0b7cbe
Eduardo <lauer.edu@gmail.com>
2023-05-14 10:38:54 -0300

    security

--- 04-Jobs-API/app.js
+++ 04-Jobs-API/app.js
@@ -4,6 +4,13 @@ const authenticateUser = require('./middleware/authentication')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+
 const connectDb = require('./db/connect')
 
 const express = require('express')
@@ -16,7 +23,15 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // app.use(express.static('./public'))
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowsMs: 15 * 60 * 1000,
+    max: 100
+}))
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 
 // routes
@@ -33,16 +48,16 @@ app.use(errorHandlerMiddleware)
 
 
 
-    const start = async () => {
-        try {
-            // connect to databased
-            await connectDb(process.env.MONGO_URI)
-            app.listen(port, () => {
-                console.log(`Server listening at http://localhost:${port}/`)
-            })
-        } catch (error) {
-            console.log(error)
-        }
+const start = async () => {
+    try {
+        // connect to databased
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server listening at http://localhost:${port}/`)
+        })
+    } catch (error) {
+        console.log(error)
     }
+}
 
 start()
