845fb8f7104cbdff03503368f6743c9887d102a8
Eduardo <lauer.edu@gmail.com>
2023-05-14 06:44:04 -0300

    Delete controller

--- 04-Jobs-API/controllers/jobs.js
+++ 04-Jobs-API/controllers/jobs.js
@@ -8,12 +8,14 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-    const { user: { userId }, params: { id: jobId } } = req
+    const { user: { userId }, params: { jobId } } = req
 
     const job = await Job.findOne({
         _id: jobId,
         createdBy: userId
     })
+
+    // essa parte do código não roda, sempre que é tentando achar um _id não existente ele joga erro 500 e não entra nesse if
     if (!job) {
         throw new NotFoundError(`No job with id:${jobId}`)
     }
@@ -28,11 +30,44 @@ const createJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job')
+    // const { company, position } = req.body
+    // const { userId } = req.user
+    // const { jobId } = req.params
+    const { body: { company, position }, user: { userId }, params: { jobId } } = req
+
+
+    if (!company || !position) {
+        throw new BadRequestError('Company or Position fields can not be empty')
+    }
+
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+
+    // essa parte do código não roda, sempre que é tentando achar um _id não existente ele joga erro 500 e não entra nesse ifi
+    if (!job) {
+        throw new NotFoundError(`No job with id:${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({ msg: job })
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const { user: { userId }, params: { jobId } } = req
+
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId
+    })
+
+    // essa parte do código não roda, sempre que é tentando achar um _id não existente ele joga erro 500 e não entra nesse ifi
+    if (!job) {
+        throw new NotFoundError(`No job with id:${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).send()
 }
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob }
--- 04-Jobs-API/routes/jobs.js
+++ 04-Jobs-API/routes/jobs.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs')
 
 router.route('/').post(createJob).get(getAllJobs)
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route('/:jobId').get(getJob).delete(deleteJob).patch(updateJob)
 
 
 module.exports = router
